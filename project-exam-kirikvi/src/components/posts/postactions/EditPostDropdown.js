eff91938ca39a9d680e4b619394c6ef090044865
Kiri M. Kvistnes <71313020+kirikvi@users.noreply.github.com>
2023-04-16 12:59:53 +0200

    Update EditPostDropdown.js

--- project-exam-kirikvi/src/components/posts/postactions/EditPostDropdown.js
+++ project-exam-kirikvi/src/components/posts/postactions/EditPostDropdown.js
@@ -1,15 +1,35 @@
 import options from "../../../icons/options.png";
 //import DropdownItem from "./DropdownItem";
-import React, {useState } from "react";
+import React, {useEffect, useRef, useState } from "react";
 import UpdatePost from "./UpdatePost";
 
 
 export default function EditPostDropdown() {
 
     const [open, setOpen ] = useState(false);
+
+    
+    //Lets the user close the dropdown update/delete form when clicking anywhere outside the form.
+    let menuRef = useRef();
+    
+    useEffect(() => {
+        let handler = (e)=>{
+            if(!menuRef.current.contains(e.target)){
+            setOpen(false);
+            console.log(menuRef.current);
+            }
+        };
+        
+        document.addEventListener("mousedown", handler);
+
+        return() => {
+            document.removeEventListener("mousedown", handler);
+        }
+    });
+
     return (
         <div>
-            <div>
+            <div ref={menuRef}>
                 <div className="menuTrigger" onClick={()=>{setOpen(!open)}}>
                     <img src={options} alt="options"></img>
                 </div>
